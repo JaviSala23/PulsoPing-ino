bdc6e1ab19c4ad71de288e425f058a18fd8e5a9c
JaviSala23 <javisala.ca@gmail.com>
2024-07-06 20:18:26 -0300

    gf

--- api/apipulso/pulso/serializers.py
+++ api/apipulso/pulso/serializers.py
@@ -6,3 +6,13 @@ class SensorReadingSerializer(serializers.ModelSerializer):
         model = SensorReading
         fields = '__all__'
         read_only_fields = ('timestamp',)
+
+    def to_representation(self, instance):
+        # Llama al método de la superclase para obtener la representación
+        representation = super().to_representation(instance)
+        
+        # Verifica si 'timestamp' está en la representación y, si es así, imprímelo en la terminal
+        if 'timestamp' in representation:
+            print(f"Accessed timestamp: {representation['timestamp']}")
+        
+        return representation
