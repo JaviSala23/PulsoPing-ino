d4a341bc797e78d76290e16a5d8c5827002f2dca
JaviSala23 <javisala.ca@gmail.com>
2024-07-09 18:40:17 -0300

    asd

--- api/apipulso/gestion/views.py
+++ api/apipulso/gestion/views.py
@@ -71,4 +71,4 @@ class TemperatureGraphView(View):
             plt.close()  # Cerrar el gráfico para liberar memoria
 
         # Renderizar el template con los gráficos generados
-        return render(request, 'temperature_graph.html', {'plots': plots})
+        return render(request, 'graficos.html', {'plots': plots})
