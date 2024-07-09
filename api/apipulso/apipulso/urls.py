880c63a69eb64baa023215d7385bc3553613fb13
JaviSala23 <javisala.ca@gmail.com>
2024-07-09 18:11:36 -0300

    asd

--- api/apipulso/apipulso/urls.py
+++ api/apipulso/apipulso/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from pulso.views import SensorReadingListCreate, SensorReadingDetail
+from gestion.views import TemperatureGraphView
 from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sensor_readings/', SensorReadingListCreate.as_view(), name='sensor_reading_list_create'),
     path('sensor_readings/<int:pk>/', SensorReadingDetail.as_view(), name='sensor_reading_detail'),
+    path('temperature-graph/', TemperatureGraphView.as_view(), name='temperature_graph'),
 ]
