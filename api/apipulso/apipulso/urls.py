8a51175f0fceceb32b37488d719c4838114f9041
JaviSala23 <javisala.ca@gmail.com>
2024-07-10 00:04:15 -0300

    asd

--- api/apipulso/apipulso/urls.py
+++ api/apipulso/apipulso/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls), 
     path('sensor_readings/', SensorReadingListCreate.as_view(), name='sensor_reading_list_create'),
     path('sensor_readings/<int:pk>/', SensorReadingDetail.as_view(), name='sensor_reading_detail'),
-    path('temperature-graph/', gestion.TemperatureGraphView, name='temperature_graph'),
+    path('temperature-graph/<int:cuenta>/<int:puerto>/', gestion.TemperatureGraphView, name='temperature_graph'),
 ]
