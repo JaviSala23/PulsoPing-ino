e4d4b7949190ece0d6d36b9a71d653c89d9426a5
Javi pc escritorio <javisala.ca@gmail.com>
2024-07-11 12:40:12 -0300

    asd

--- api/apipulso/gunicorn_config.py
+++ api/apipulso/gunicorn_config.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
 # Nombre del módulo WSGI de tu proyecto Django
-bind = 'unix:/path/to/your/django/project/yourproject.sock'
+bind = 'unix:/home/PulsoPing-ino/api/apipulso/PulsoPing-ino.sock'
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 30
