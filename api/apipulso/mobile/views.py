4b701825588c3d4f797fea2abaf618f6380bf4ae
JaviSala23 <javisala.ca@gmail.com>
2024-08-06 23:18:50 -0300

    mobile

--- api/apipulso/mobile/views.py
+++ api/apipulso/mobile/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-
+from gestion.models import Cuenta_has_Artefacto
+from gestion.views import obtener_ultimo_registro
 
 def panel_view(request):
-    return render(request, 'mobile/panel.html')
+
+    relaciones = Cuenta_has_Artefacto.objects.all()
+
+    # Lista para almacenar relaciones junto con sus últimos registros de temperatura
+    relaciones_actualizadas = []
+
+    # Itera sobre las relaciones para obtener y agregar el último registro de temperatura
+    for relacion in relaciones:
+        archivo_path = relacion.url  # Ajusta esto según tu modelo y campo correspondiente
+        try:
+            ultimo_registro = obtener_ultimo_registro(archivo_path)
+        except:
+            ultimo_registro=0
+        # Agrega un diccionario con la relación y el último registro de temperatura
+        relaciones_actualizadas.append({
+            'relacion': relacion,
+            'ultimo_registro': ultimo_registro
+        })
+
+    return render(request, 'mobile/panel.html', {'relaciones_actualizadas': relaciones_actualizadas})
+    
