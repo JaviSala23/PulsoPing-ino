983890749a776f8e17aa9369a7ef1bca7f6ed2d5
JaviSala23 <52177563+JaviSala23@users.noreply.github.com>
2024-08-09 18:07:41 -0300

    asas

--- api/apipulso/pulso/admin.py
+++ api/apipulso/pulso/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Firmware, Placa, SensorReading, MessageLog
 
-# Register your models here.
+@admin.register(Firmware)
+class FirmwareAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'version', 'descripcion')
+    search_fields = ('nombre', 'version')
+    list_filter = ('nombre',)
+
+@admin.register(Placa)
+class PlacaAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'descripcion', 'firmware')
+    search_fields = ('codigo', 'descripcion')
+    list_filter = ('firmware',)
+
+@admin.register(SensorReading)
+class SensorReadingAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'temperature', 'placa', 'puerto', 'compresor_status', 'puerta_status')
+    search_fields = ('placa__codigo', 'puerto')
+    list_filter = ('compresor_status', 'puerta_status')
+    date_hierarchy = 'timestamp'
+
+@admin.register(MessageLog)
+class MessageLogAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'placa', 'puerto', 'message_type', 'temperature', 'compresor_status', 'puerta_status')
+    search_fields = ('placa__codigo', 'message_type')
+    list_filter = ('message_type', 'compresor_status', 'puerta_status')
+    date_hierarchy = 'timestamp'
