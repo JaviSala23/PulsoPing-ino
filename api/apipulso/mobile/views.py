b0c1ab7126992dc439e552fc0cbad09232c673eb
JaviSala23 <52177563+JaviSala23@users.noreply.github.com>
2024-08-19 15:57:18 -0300

    asas

--- api/apipulso/mobile/views.py
+++ api/apipulso/mobile/views.py
@@ -146,20 +146,17 @@ def translate_timestamp(timestamp):
     # Función para traducir el timestamp a español
     return timestamp.strftime('%d/%m %H:%M')
 
-def TemperatureGraphView(request, id):
+def TemperatureGraphView(request, cuenta, puerto):
     try:
-        # Obtener el objeto Cuenta_has_Artefacto correspondiente
-        artefacto1 = Cuenta_has_Artefacto.objects.get(pk=id)
+        artefacto1 = Cuenta_has_Artefacto.objects.get(cuenta=cuenta, puerto=puerto)
     except Cuenta_has_Artefacto.DoesNotExist:
         return HttpResponse("No se encontró el artefacto especificado.", content_type="text/plain")
     
     data = []
     
     try:
-        # Limpiar la URL para evitar caracteres de nueva línea
         url_clean = artefacto1.url.strip()
 
-        # Leer datos del archivo
         with open(url_clean, 'r') as f:
             lines = f.readlines()
     except Exception as e:
@@ -167,30 +164,30 @@ def TemperatureGraphView(request, id):
 
     for line in lines:
         parts = line.strip().split(',')
-        if len(parts) < 2:
+        if len(parts) < 6:  # Asegúrate de que haya al menos 6 partes en la línea
             continue
 
         try:
             timestamp = datetime.strptime(parts[0], "%Y-%m-%d %H:%M:%S")
             temperature = float(parts[1])
-            data.append((timestamp, temperature))
+            puerta = 1 if parts[4].strip().lower() == 'true' else 0
+            compresor = 1 if parts[5].strip().lower() == 'true' else 0
+            data.append((timestamp, temperature, puerta, compresor))
         except Exception as e:
             continue
 
-    # Convertir los datos en un DataFrame de pandas
     if not data:
         return HttpResponse("No se encontraron datos válidos en los archivos.", content_type="text/plain")
 
-    df = pd.DataFrame(data, columns=['timestamp', 'temperature'])
+    df = pd.DataFrame(data, columns=['timestamp', 'temperature', 'puerta', 'compresor'])
 
-    # Filtrar datos por hora y fecha si se especifican en los parámetros de la solicitud
+    # Filtrar por fecha y hora
     fecha_inicio_str = request.GET.get('fecha_inicio', None)
     fecha_fin_str = request.GET.get('fecha_fin', None)
 
     if not fecha_inicio_str and not fecha_fin_str:
-        # Calcular fecha_inicio y fecha_fin si no están especificadas
-        fecha_fin = datetime.now()  # Fecha actual
-        fecha_inicio = fecha_fin - timedelta(hours=4)  # 4 horas antes de la fecha actual
+        fecha_fin = datetime.now()
+        fecha_inicio = fecha_fin - timedelta(hours=4)
     else:
         try:
             fecha_inicio = datetime.fromisoformat(fecha_inicio_str)
@@ -200,34 +197,35 @@ def TemperatureGraphView(request, id):
 
     df = df[(df['timestamp'] >= fecha_inicio) & (df['timestamp'] <= fecha_fin)]
 
-    
-
-    # Preparar datos para el gráfico
-    timestamps = [translate_timestamp(ts) for ts in df['timestamp']]
-    temperatures = df['temperature'].tolist()
-
-      # Ordenar los datos de más recientes a más antiguos
     df = df.sort_values(by='timestamp', ascending=False)
 
-    # Preparar datos para la tabla
-    table_data = []
-    for date, temp in zip(df['timestamp'], df['temperature']):
-        date_str = translate_timestamp(date)  # Traducir la fecha y hora a español
-        
-        # Determinar el color de la temperatura en la tabla según los rangos definidos
-        if temp < artefacto1.temp_min or temp > artefacto1.temp_max:
-            temp_color = 'red'
-        else:
-            temp_color = 'blue'
-        
-        table_data.append({'fecha_hora': date_str, 'temperatura': temp, 'color': temp_color})
+    timestamps = [translate_timestamp(ts) for ts in df['timestamp']]
+    temperatures = df['temperature'].tolist()
+    puerta_states = df['puerta'].tolist()
+    compresor_states = df['compresor'].tolist()
 
-    # Renderizar la plantilla con los datos del gráfico
-    return render(request, 'mobile/estadisticaM.html', {
+    table_data = []
+    for date, temp, puerta, compresor in zip(df['timestamp'], df['temperature'], df['puerta'], df['compresor']):
+        date_str = translate_timestamp(date)
+        
+        # Determina el color basado en los límites de temperatura
+        temp_color = 'red' if temp < artefacto1.temp_min or temp > artefacto1.temp_max else 'blue'
+        
+        table_data.append({
+            'fecha_hora': date_str, 
+            'temperatura': temp, 
+            'color': temp_color, 
+            'puerta': 'Abierta' if puerta == 1 else 'Cerrada', 
+            'compresor': 'Encendido' if compresor == 1 else 'Apagado'
+        })
+
+    return render(request, 'monitoreo/graficos.html', {
         'timestamps': json.dumps(timestamps),
         'temperatures': json.dumps(temperatures),
+        'puerta_states': json.dumps(puerta_states),
+        'compresor_states': json.dumps(compresor_states),
         'cuenta': artefacto1.cuenta.nombre_cuenta,
-        'puerto': artefacto1.puerto,
+        'puerto': puerto,
         'descripcion': artefacto1.artefacto.descripcion,
         'tabla_datos': table_data,
         'datos': artefacto1
