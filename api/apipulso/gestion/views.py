5ae731d0798049059b3f5f4d7414746e4764477a
JaviSala23 <52177563+JaviSala23@users.noreply.github.com>
2024-08-20 10:58:20 -0300

    aaa

--- api/apipulso/gestion/views.py
+++ api/apipulso/gestion/views.py
@@ -216,7 +216,7 @@ def obtener_ultimo_registro(archivo_path):
             lines = file.readlines()
             if lines:
                 last_line = lines[-1].strip()
-                date_str, temp_str, placa, puerto, compresor, puerta = last_line.split(',')
+                date_str, temp_str, placa, puerto, puerta,compresor = last_line.split(',')
                 ultimo_registro = {
                     'fecha_hora': parse_datetime(date_str),
                     'temperatura': float(temp_str),
