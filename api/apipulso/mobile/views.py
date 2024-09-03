33699fcab2c6bac745809f4a47853954b5e49a07
JaviSala23 <52177563+JaviSala23@users.noreply.github.com>
2024-09-02 22:24:35 -0300

    asa

--- api/apipulso/mobile/views.py
+++ api/apipulso/mobile/views.py
@@ -32,6 +32,7 @@ from django.contrib.auth import logout
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 def login_view(request):
     # Obtener el nombre de usuario
