b8839dab8ff1429af1b41038ec9968eb0be4aec1
JaviSala23 <52177563+JaviSala23@users.noreply.github.com>
2024-10-01 17:38:36 -0300

    aa

--- api/apipulso/pulso/views.py
+++ api/apipulso/pulso/views.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 import pytz
 
 class SensorReadingListCreate(generics.ListCreateAPIView):
-    #queryset = SensorReading.objects.all()
+    queryset = SensorReading.objects.order_by('-timestamp').first()
     serializer_class = SensorReadingSerializer 
 
     def perform_create(self, serializer):
