08dd0bce68f45089779969fadeb6a399719a7f5a
JaviSala23 <javisala.ca@gmail.com>
2024-10-02 00:26:56 -0300

    serializer

--- api/apipulso/pulso/serializers.py
+++ api/apipulso/pulso/serializers.py
@@ -2,21 +2,25 @@ from rest_framework import serializers
 from .models import SensorReading, Placa
 
 class PlacaSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Placa
         fields = ['id', 'codigo']  # Ajusta los campos según tus necesidades
 
 class SensorReadingSerializer(serializers.ModelSerializer):
     placa = serializers.PrimaryKeyRelatedField(queryset=Placa.objects.all())
+    compresor_status = serializers.BooleanField(required=False, allow_null=True)
+    puerta_status = serializers.BooleanField(required=False, allow_null=True)
+    energia_status = serializers.BooleanField(required=False, allow_null=True)
 
     class Meta:
         model = SensorReading
-        fields = ['temperature', 'placa', 'puerto', 'timestamp', 'compresor_status', 'puerta_status','energia_status']
+        fields = ['temperature', 'placa', 'puerto', 'timestamp', 'compresor_status', 'puerta_status', 'energia_status']
         read_only_fields = ('timestamp',)
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        print(f"Lectura temperatura: {representation['temperature']}, Placa: {representation['placa']}, Puerto: {representation['puerto']}, Puerta: {representation['puerta_status']}, Compresor: {representation['compresor_status']},Energia: {representation['energia_status']}")
+        print(f"Lectura temperatura: {representation['temperature']}, Placa: {representation['placa']}, Puerto: {representation['puerto']}, Puerta: {representation.get('puerta_status')}, Compresor: {representation.get('compresor_status')}, Energia: {representation.get('energia_status')}")
         return representation
 
     def create(self, validated_data):
